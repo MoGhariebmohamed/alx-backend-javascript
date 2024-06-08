7fe575b89866f2fb8d965e85063921ae32e66bf4
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-09 02:35:51 +0300

    t11v3

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {[$departmentName]:[
-  ...employees,
-  ]
+  return {
+    [departmentName]: [
+      ...employees,
+  ],
 };
 }
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
