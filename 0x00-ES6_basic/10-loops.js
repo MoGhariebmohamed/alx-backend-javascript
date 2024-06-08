a7f1d599f0c6ba92b29e9bf3bd059ae8bb056c71
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-09 02:26:09 +0300

    t10v5

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = [];
-  for (let idx of array) {
+  const newArray = [];
+  for (const idx of array) {
     newArray.push(appendString + idx);
   }
 
   return newArray;
 }
-console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
