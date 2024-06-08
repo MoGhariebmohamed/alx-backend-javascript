751510a9fa1fac1e7e168614a525b5219a5ba6c1
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-09 02:32:58 +0300

    t11v5

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,17 +1,6 @@
-import getBudgetObject from './7-getBudgetObject';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-
-  
-  return fullBudget;
+export default function createEmployeesObject(departmentName, employees) {
+  return {[$departmentName]:[
+  ...employees,
+  ]
+};
 }
