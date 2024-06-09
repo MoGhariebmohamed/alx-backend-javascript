f5d1e4a9a6dde1c399470ee28cb2043129648efc
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-09 03:29:45 +0300

    t13v3

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const itr = 1,
-  employeeArr = [];
-    for (itr of reportWithIterator) {
-    employeeArr.push(itr)
+  let itr = 1;
+  const employeeArr = [];
+  for (itr of reportWithIterator) {
+    employeeArr.push(itr);
   }
-  return employeeArr.join(' | ')
+  return employeeArr.join(' | ');
 }
