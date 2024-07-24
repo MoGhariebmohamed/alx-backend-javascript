fb93da6c0e44a29020c8e4615a9c18bdf6f2e70b
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-24 05:26:56 +0300

    task 1 rev 3

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,16 @@
-function displayMessage(stdOut) {
-    console.log(stdOut);
-}
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-module.exports = displayMessage;
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
